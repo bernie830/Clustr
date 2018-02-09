d8068aa4354e96ce118bca579eb47693c5a1283c
bernie830 <hunterbernhardt@aol.com>
2018-02-09 13:51:36 -0500

    Added more DB files

---
+++ app/src/main/java/com/nothing/hunnaz/clustr/EventDBHelper.java
@@ -0,0 +1,48 @@
+package com.example.hunterbernhardt.myapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.util.Log;
+
+import com.example.hunterbernhardt.myapplication.EventDBSchema.AccountsTable;
+
+/**
+ * Created by hunterbernhardt on 2/9/18.
+ */
+public final class EventDBHelper extends SQLiteOpenHelper {
+    /* All taken from TicTacToe */
+    private Context mContext;
+    private static final String DATABASE_NAME = "Clustr.db";
+    private static final int DATABASE_VERSION = 1;
+
+    // Class name for logging.
+    private final String TAG = getClass().getSimpleName();
+
+    public EventDBHelper(Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+        sqLiteDatabase.execSQL("CREATE TABLE " + AccountsTable.NAME + "(" +
+                "_id INTEGER PRIMARY KEY AUTOINCREMENT, " +
+                AccountsTable.Cols.TITLE + " TEXT, " +
+                AccountsTable.Cols.LOCATION + " TEXT," +
+                AccountsTable.Cols.CAPACITY + " INTEGER, " +
+                AccountsTable.Cols.DATE + " TEXT," +
+                AccountsTable.Cols.DESCRIPTION + " TEXT, " +
+                AccountsTable.Cols.COST + " REAL," +
+                AccountsTable.Cols.AGE + " INTEGER," +
+                AccountsTable.Cols.CREATOR + " INTEGER" +
+                ")");
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int oldVersion, int newVersion) {
+        Log.w("Example", "Example: upgrading database; dropping and recreating tables.");
+        sqLiteDatabase.execSQL("DROP TABLE IF EXISTS " + AccountsTable.NAME);
+        onCreate(sqLiteDatabase);
+    }
+}
---
+++ app/src/main/java/com/nothing/hunnaz/clustr/EventDBSchema.java
@@ -0,0 +1,25 @@
+package com.example.hunterbernhardt.myapplication;
+
+/**
+ * Created by hunterbernhardt on 2/9/18.
+ */
+
+public class EventDBSchema {
+    public static final class AccountsTable {
+        /* All taken from TicTacToe then edited */
+        public static final String NAME = "events";
+
+        public static final class Cols {
+            public static final String TITLE = "title";
+            public static final String LOCATION = "location";
+            public static final String CAPACITY = "capacity";
+            public static final String DATE = "date";
+            public static final String DESCRIPTION = "description";
+            public static final String COST = "cost";
+            public static final String AGE = "age";
+            public static final String CREATOR = "creator";
+
+            // public static final String IMAGE = "password";
+        }
+    }
+}
