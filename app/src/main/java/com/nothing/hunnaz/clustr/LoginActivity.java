74fb92517c85d10897d4bb000e23e6794d574106
bernie830 <hunterbernhardt@aol.com>
2018-02-19 16:41:38 -0500

    Got the login screen into a clean looking single fragment activity -- Only the register screen remaining

--- app/src/main/java/com/nothing/hunnaz/clustr/LoginActivity.java
+++ app/src/main/java/com/nothing/hunnaz/clustr/LoginActivity.java
@@ -2,6 +2,7 @@ package com.nothing.hunnaz.clustr;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.ViewGroup;
@@ -14,65 +15,10 @@ import com.nothing.hunnaz.clustr.UserDB.UserSingleton;
 
 import java.util.List;
 
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
-    private EditText usernameTextEntry;
-    private EditText passwordTextEntry;
-    private TextView informationText;
-    private Intent nextScreen;
+public class LoginActivity extends SingleFragmentActivity{
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_login);
-
-    }
-
-    // Return an error message if there is one
-    private String attemptLogin(String username, String password){
-        UserSingleton singleton = UserSingleton.get(this.getApplicationContext());
-        boolean validLogin = singleton.isValidLogin(username, password);
-        String retVal = "";
-        if(validLogin) {
-            UserPrefs.logInUser(username, this);
-            startActivity(nextScreen);
-        } else {
-            retVal = "The entered information was invalid. Please try again.";
-        }
-        return retVal;
-    }
-
-    private void changeToRegister(){
-        nextScreen = new Intent(this, RegisterActivity.class);
-        startActivity(nextScreen);
-    }
-
-    private void returnToHome(){
-        startActivity(nextScreen);
-    }
-
-    @Override
-    public void onClick(View view) {
-        ViewGroup v = (ViewGroup) view.getParent();
-        usernameTextEntry = (EditText) v.findViewById(R.id.userName);
-        passwordTextEntry = (EditText) v.findViewById(R.id.password);
-        informationText = (TextView) v.findViewById(R.id.loginInfo);
-
-        //Because from here we only ever want to return to the home screen
-        nextScreen = new Intent(this, HomeActivity.class);
-
-        switch (view.getId()) {
-            case R.id.doneButton:
-                String newInfo = attemptLogin(usernameTextEntry.getText().toString(), passwordTextEntry.getText().toString());
-                if(newInfo.length() != 0) {
-                    informationText.setText(newInfo);
-                }
-                break;
-            case R.id.registerButton:
-                changeToRegister();
-                break;
-            case R.id.backButton:
-                returnToHome();
-                break;
-        }
+    protected Fragment createFragment() {
+        return new LoginFragment();
     }
 }
---
+++ app/src/main/java/com/nothing/hunnaz/clustr/LoginFragment.java
@@ -0,0 +1,107 @@
+package com.nothing.hunnaz.clustr;
+
+import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import com.nothing.hunnaz.clustr.UserDB.UserSingleton;
+
+
+/**
+ *
+ */
+public class LoginFragment extends Fragment implements View.OnClickListener{
+    private EditText usernameTextEntry;
+    private EditText passwordTextEntry;
+    private TextView informationText;
+    private Intent nextScreen;
+
+    // Return an error message if there is one
+    private String attemptLogin(String username, String password){
+        UserSingleton singleton = UserSingleton.get(this.getContext());
+        boolean validLogin = singleton.isValidLogin(username, password);
+        String retVal = "";
+        if(validLogin) {
+            UserPrefs.logInUser(username, this.getContext());
+            startActivity(nextScreen);
+        } else {
+            retVal = "The entered information was invalid. Please try again.";
+        }
+        return retVal;
+    }
+
+    private void changeToRegister(){
+        nextScreen = new Intent(this.getContext(), RegisterActivity.class);
+        startActivity(nextScreen);
+    }
+
+    private void returnToHome(){
+        startActivity(nextScreen);
+    }
+
+    @Override
+    public void onClick(View view) {
+        ViewGroup v = (ViewGroup) view.getParent();
+        usernameTextEntry = (EditText) v.findViewById(R.id.userName);
+        passwordTextEntry = (EditText) v.findViewById(R.id.password);
+        informationText = (TextView) v.findViewById(R.id.loginInfo);
+
+        //Because from here we only ever want to return to the home screen
+        nextScreen = new Intent(this.getContext(), HomeActivity.class);
+
+        switch (view.getId()) {
+            case R.id.doneButton:
+                String newInfo = attemptLogin(usernameTextEntry.getText().toString(), passwordTextEntry.getText().toString());
+                if(newInfo.length() != 0) {
+                    informationText.setText(newInfo);
+                }
+                break;
+            case R.id.registerButton:
+                changeToRegister();
+                break;
+            case R.id.backButton:
+                returnToHome();
+                break;
+        }
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        View v = inflater.inflate(R.layout.fragment_login, container, false);
+        int rotation = getActivity().getWindowManager().getDefaultDisplay().getRotation();
+
+
+        Button btnAdd = (Button) v.findViewById(R.id.backButton);
+        btnAdd.setOnClickListener(this);
+
+        btnAdd = (Button) v.findViewById(R.id.doneButton);
+        btnAdd.setOnClickListener(this);
+
+        btnAdd = (Button) v.findViewById(R.id.registerButton);
+        btnAdd.setOnClickListener(this);
+
+        return v;
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+    }
+
+    @Override
+    public void onDetach() {
+        super.onDetach();
+    }
+}
