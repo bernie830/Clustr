b24b3374676b98cf3f635ae2eec8d9d352beb1c9
bernie830 <hunterbernhardt@aol.com>
2018-03-07 13:43:45 -0500

    Startign validation but not perfect yet

--- app/src/main/java/com/nothing/hunnaz/clustr/AddEventFragment.java
+++ app/src/main/java/com/nothing/hunnaz/clustr/AddEventFragment.java
@@ -2,6 +2,7 @@ package com.nothing.hunnaz.clustr;
 
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -9,20 +10,119 @@ import android.view.Surface;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 /**
  *
  */
 public class AddEventFragment extends Fragment implements View.OnClickListener {
+
+    EditText name;
+    EditText description;
+    EditText cost;
+    EditText age;
+    EditText date;
+    EditText capacity;
+    EditText address;
+    TextView information;
+
+
     private void switchIntent(Class name){
         Intent myIntent = new Intent(this.getContext(), name);
         startActivity(myIntent);
     }
 
+    // TODO - Does nothing until the DB is implemented
+    private void addEventToDatabase(String name, String des, String cost, String age, String date, String capacity, String address){
+
+    }
+
+    private static String validateName(String name){
+        String retVal = "";
+        int len = name.length();
+        boolean b = (len == 0);
+        if(b) { retVal = "ERROR: A name must be entered for the event"; }
+        return retVal;
+    }
+
+    // TODO - Need to do this
+    private static String validateCost(String cost){
+        String retVal = "";
+        if(cost.length() == 0){
+            retVal = "ERROR: A name must be entered for the event";
+        }
+        return retVal;    }
+
+    private static String validateAge(String age){
+        String retVal = "";
+        int ageNum = Integer.parseInt(age);
+        if(ageNum < 1 || ageNum > 100){
+            retVal = "ERROR: The age requirement must be between 1 and 100";
+        }
+        return retVal;
+    }
+
+    private static String validateDate(String date){
+        String retVal = "";
+        Date day = new Date(date);
+        if(!day.confirmDate()){
+            retVal = "ERROR: The entered date is not valid";
+        }
+        return retVal;    }
+
+    private static String validateCap(String capacity){
+        String retVal = "";
+        int capNum = Integer.parseInt(capacity);
+        if(capNum < 10){
+            retVal = "ERROR: The age requirement must be one or more";
+        }
+        return retVal;    }
+
+    private static String validateAddress(String address){
+        String retVal = "";
+        if(address.length() == 0) {
+            retVal = "ERROR: An address must be entered for the event";
+        }
+        return retVal;    }
+
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
+            case R.id.backButton:
+                break;
             case R.id.doneButton:
+                String nameStr = name.getText().toString();
+                String descriptionStr = description.getText().toString();
+                String costStr = cost.getText().toString();
+                String ageStr = age.getText().toString();
+                String dateStr = date.getText().toString();
+                String capacityStr = capacity.getText().toString();
+                String addressStr = address.getText().toString();
+                String errorMessage = validateName(nameStr);
+                if(errorMessage.length() == 0){
+                    errorMessage = validateCost(costStr);
+                }
+                if(errorMessage.length() == 0){
+                    errorMessage = validateAge(ageStr);
+                }
+                if(errorMessage.length() == 0){
+                    errorMessage = validateDate(dateStr);
+                }
+                if(errorMessage.length() == 0){
+                    errorMessage = validateCap(capacityStr);
+                }
+                if(errorMessage.length() == 0){
+                    errorMessage = validateAddress(capacityStr);
+                }
+                // Assume description can be empty
+                if(errorMessage.length() == 0) {
+                    addEventToDatabase(nameStr, descriptionStr, costStr, ageStr, dateStr, capacityStr, addressStr);
+                    switchIntent(HomeActivity.class);
+                } else {
+                    information.setText(errorMessage);
+                    information.setTextColor(Color.RED);
+                }
                 break;
             default:
                 break;
@@ -40,6 +140,19 @@ public class AddEventFragment extends Fragment implements View.OnClickListener {
 
         Button btnAdd = (Button) v.findViewById(R.id.backButton);
         btnAdd.setOnClickListener(this);
+
+        btnAdd = (Button) v.findViewById(R.id.doneButton);
+        btnAdd.setOnClickListener(this);
+
+        information = (TextView) v.findViewById(R.id.addInfo);
+
+        name = (EditText) v.findViewById(R.id.eventName);
+        description = (EditText) v.findViewById(R.id.description);
+        cost = (EditText) v.findViewById(R.id.cost);
+        age = (EditText) v.findViewById(R.id.age);
+        date = (EditText) v.findViewById(R.id.date);
+        capacity = (EditText) v.findViewById(R.id.capacity);
+        address = (EditText) v.findViewById(R.id.address);
 
         return v;
     }
